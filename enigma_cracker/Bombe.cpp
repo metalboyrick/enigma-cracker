9ee27b3528da87877c3ca97e9677b103ee74bafa
DESKTOP-S551RPR\Richard <rsulisthio@gmail.com>
2021-04-05 12:34:44 +0700

    Added iterations for the cracker

--- enigma_cracker/Bombe.cpp
+++ enigma_cracker/Bombe.cpp
@@ -1,6 +1,42 @@
 #include "Bombe.h"
+#include "Utility.h"
+#include <iostream>
 
 Bombe::Bombe(std::vector<std::vector<LoopEdge>> loops)
 {
 	loops = loops;
+}
+
+void Bombe::crack()
+{
+	std::vector<std::vector<int>> rotorCombinations = getPermutation({1,2,3,4,5}, 3);
+	
+	// iterate through the rotor combinations
+	for (auto& rotorSetting : rotorCombinations) {
+
+		// iterate through he rotor init positions
+		for (int leftRotorPos = 0; leftRotorPos < 26; leftRotorPos++) {
+			for (int centerRotorPos = 0; centerRotorPos < 26; centerRotorPos++) {
+				for (int rightRotorPos = 0; rightRotorPos < 26; rightRotorPos++) {
+
+					// iterate through all loops
+					for (auto& loop : loops) {
+
+						// set up the enigma machine
+						enigma.rotorSettings = rotorSetting;
+						enigma.rotorPosition = std::string({indexToChar(leftRotorPos), 
+															indexToChar(centerRotorPos),
+															indexToChar(rightRotorPos),
+															'\0'});
+
+
+
+						// TODO
+						continue;
+					}
+				}
+			}
+		}
+	}
+
 }
